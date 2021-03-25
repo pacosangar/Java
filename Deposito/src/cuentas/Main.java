e6b97804d60512fe47b9dca7909cd05c2b0cc38a
FSG <FSG@DESKTOP-9L068DO.mshome.net>
2021-03-25 13:05:43 +0100

    Refactoring: Definimos parametros para método operativa_cuenta. Utilizamos switch para que decidir que operación se reaiza en función de parametro recibido.

--- Deposito/src/cuentas/Main.java
+++ Deposito/src/cuentas/Main.java
@@ -1,32 +1,37 @@
 package cuentas;
+
 public class Main {
-	
-	public static void operativa_cuenta(CCuenta cuenta1) {
-		
 
-        System.out.println("El saldo actual es "+ cuenta1.getSaldo() );
+	public static void operativa_cuenta(CCuenta cuenta1, String operacion, float cantidad) {
 
-        try {
-            cuenta1.retirar(2300);
-            System.out.println("RETIRAR -> El saldo actual es "+ cuenta1.getSaldo() );
-        } catch (Exception e) {
-            System.out.print("Fallo al retirar");
-        }
-        try {
-            System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
-            System.out.println("INGRESO -> El saldo actual es "+ cuenta1.getSaldo() );
-        } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
-        }
-		
+		System.out.println("El saldo actual es " + cuenta1.getSaldo());
+
+		switch (operacion) {
+		case ("retirar"):
+			try {
+				cuenta1.retirar(cantidad);
+				System.out.println("RETIRAR -> El saldo actual es " + cuenta1.getSaldo());
+
+			} catch (Exception e) {
+				System.out.print("Fallo al retirar");
+			};
+			break;
+		case ("ingresar"):
+			try {
+				System.out.println("Ingreso en cuenta");
+				cuenta1.ingresar(cantidad);
+				System.out.println("INGRESO -> El saldo actual es " + cuenta1.getSaldo());
+			} catch (Exception e) {
+				System.out.print("Fallo al ingresar");
+			};
+			break;
+		}
 	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         CCuenta cuenta1;
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        operativa_cuenta(cuenta1);
-        
-        
+        operativa_cuenta(cuenta1, "retirar", 2300);
+        operativa_cuenta(cuenta1, "ingresar", 695);
     }
 }
