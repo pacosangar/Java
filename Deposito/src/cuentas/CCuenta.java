51bd2a136aa498e10d8c19c7788492600f97bd97
FSG <FSG@DESKTOP-9L068DO.mshome.net>
2021-03-25 11:07:20 +0100

    Initial commit

---
+++ Deposito/src/cuentas/CCuenta.java
@@ -0,0 +1,70 @@
+package cuentas;
+
+public class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInteres;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
+        setTipoInteres(tipo);
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (getSaldo()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+    
+    public String getNombre() {
+    	return nombre;
+    }
+    
+    public void setNombre(String inputNombre) {
+    	nombre = inputNombre;
+    }
+    
+    public String getCuenta() {
+    	return cuenta;
+    }
+    
+    public void setCuenta(String inputCuenta) {
+    	cuenta = inputCuenta;
+    }
+    
+    public double getSaldo() {
+    	return saldo;
+    }
+    
+    public void setSaldo(double inputSaldo) {
+    	saldo = inputSaldo;
+    }
+    
+    public double getTipoInteres() {
+    	return tipoInteres;
+    }
+    
+    public void setTipoInteres(double inputTipoInteres) {
+    	tipoInteres = inputTipoInteres;
+    }
+}
