d1f5e05da28a19a9d02b3bd252b754ee05019347
FSG <FSG@DESKTOP-9L068DO.mshome.net>
2021-03-30 17:44:01 +0200

    Mover el método opertivaCuenta, en la clase CCuenta.

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -2,70 +2,98 @@ package cuentas;
 
 public class CCuenta {
 
+	/* Atributos de mi clase */
+	private String nombre;
+	private String cuenta;
+	private double saldo;
+	private double tipoInteres;
 
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInteres;
+	public CCuenta() {
+		System.out.println("Constructor cuenta 2");
+	}
 
-    public CCuenta()
-    {
-    	System.out.println("Constructor cuenta 2");
-    }
+	public CCuenta(String inNombre, String inCuenta, double inSaldo, double inTipoInteres) {
+		this.setNombre(inNombre);
+		this.setCuenta(inCuenta);
+		this.setSaldo(inSaldo);
+		this.setTipoInteres(inTipoInteres);
+	}
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        setNombre(nom);
-        setCuenta(cue);
-        setSaldo(sal);
-        setTipoInteres(tipo);
-    }
+	public void operativa_cuenta(String operacion, float cantidad) {
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        	setSaldo(getSaldo() + cantidad);
-    }
+		System.out.println("El saldo actual es " + this.getSaldo());
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (getSaldo()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
-        	setSaldo(getSaldo() - cantidad);
-    }
-    
-    public String getNombre() {
-    	return nombre;
-    }
-    
-    public void setNombre(String inputNombre) {
-    	nombre = inputNombre;
-    }
-    
-    public String getCuenta() {
-    	return cuenta;
-    }
-    
-    public void setCuenta(String inputCuenta) {
-    	cuenta = inputCuenta;
-    }
-    
-    public double getSaldo() {
-    	return saldo;
-    }
-    
-    public void setSaldo(double inputSaldo) {
-    	saldo = inputSaldo;
-    }
-    
-    public double getTipoInteres() {
-    	return tipoInteres;
-    }
-    
-    public void setTipoInteres(double inputTipoInteres) {
-    	tipoInteres = inputTipoInteres;
-    }
+		switch (operacion) {
+		case ("retirar"):
+			try {
+				this.retirar(cantidad);
+				System.out.println("RETIRAR " + cantidad + " -> El saldo actual es " + this.getSaldo());
+
+			} catch (Exception e) {
+				System.out.print("Fallo al retirar");
+			}
+			;
+			break;
+		case ("ingresar"):
+			try {
+				// System.out.println("Ingreso en cuenta");
+				this.ingresar(cantidad);
+				System.out.println("INGRESO " + cantidad + "-> El saldo actual es " + this.getSaldo());
+			} catch (Exception e) {
+				System.out.print("Fallo al ingresar");
+			}
+			;
+			break;
+		default:
+			System.out.println("Operación no VALIDA");
+			break;
+		}// Fin de switch
+
+	}
+
+	public void ingresar(double cantidad) throws Exception {
+		if (cantidad < 0)
+			throw new Exception("No se puede ingresar una cantidad negativa");
+		this.setSaldo(this.getSaldo() + cantidad);
+	}
+
+	public void retirar(double cantidad) throws Exception {
+		if (cantidad <= 0)
+			throw new Exception("No se puede retirar una cantidad negativa");
+		if (this.getSaldo() < cantidad)
+			throw new Exception("No se hay suficiente saldo");
+		this.setSaldo(this.getSaldo() - cantidad);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String inputNombre) {
+		nombre = inputNombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String inputCuenta) {
+		cuenta = inputCuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double inputSaldo) {
+		saldo = inputSaldo;
+	}
+
+	public double getTipoInteres() {
+		return tipoInteres;
+	}
+
+	public void setTipoInteres(double inputTipoInteres) {
+		tipoInteres = inputTipoInteres;
+	}
 }
--- Deposito/src/cuentas/Main.java
+++ Deposito/src/cuentas/Main.java
@@ -1,37 +1,18 @@
 package cuentas;
 
+/**
+ * 
+ * PRACTICA DEL TEMA 4. OPTIMIACIÓN Y DOCUMENTACIÓN
+ * 
+ */
+
 public class Main {
 
-	public static void operativa_cuenta(CCuenta cuenta1, String operacion, float cantidad) {
-
-		System.out.println("El saldo actual es " + cuenta1.getSaldo());
-
-		switch (operacion) {
-		case ("retirar"):
-			try {
-				cuenta1.retirar(cantidad);
-				System.out.println("RETIRAR " + cantidad + " -> El saldo actual es " + cuenta1.getSaldo());
-
-			} catch (Exception e) {
-				System.out.print("Fallo al retirar");
-			};
-			break;
-		case ("ingresar"):
-			try {
-				//System.out.println("Ingreso en cuenta");
-				cuenta1.ingresar(cantidad);
-				System.out.println("INGRESO " + cantidad +  "-> El saldo actual es " + cuenta1.getSaldo());
-			} catch (Exception e) {
-				System.out.print("Fallo al ingresar");
-			};
-			break;
-		}
-	}
-
 	public static void main(String[] args) {
-        CCuenta cuenta1;
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        operativa_cuenta(cuenta1, "retirar", 2300);
-        operativa_cuenta(cuenta1, "ingresar", 695);
-    }
+		CCuenta cuenta1;
+		cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+		cuenta1.operativa_cuenta("retirar", 2300);
+		cuenta1.operativa_cuenta("ingresar", 695);
+		cuenta1.operativa_cuenta("ingreso", 695);
+	}
 }
